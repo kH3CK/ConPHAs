cbf9b7702791663259e8dca8f5cc9410286f7295
Jason van Ark <jpvanark@st.noorderpoort.nl>
2025-03-11 09:27:59 +0100

    navbar

---
+++ template/views/over.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once("../template/components/head.php");
+
+?>
+<body>
+<div class="bg-[url('/public/images/bg.png')] bg-cover bg-center h-screen w-full relative">
+    <img src="../public/images/bg.png" class="absolute inset-0 w-full h-full object-cover" alt="line">
+</div>
+<div class="relative inline-block group">
+    <img src="../public/images/fles.png" alt="fles" class="hover-trigger">
+    <div class="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-100 bg-black text-white text-center rounded-lg py-1 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+    Plastic word voor van alles gebruikt en vervuilt onze aarde. Dit is natuurlijk niet goed, daarom maken wij een materiaal wat lijkt op plastic. Dit is gemaakt van Polyhydroxyalkanoaten (PHA's). Die worden in de natuur geproduceerd door een grote hoeveelheid aan micro-organismen, waaronder bacteriën. Hiermee kan bioplastic gemaakt worden en dit is biologisch afbreekbaar.
+    </div>
+</div>
+</body>
