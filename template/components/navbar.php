755aacd24a3141f8ddea1c715a522f70d2336e73
Oleksandr Antonov <ooantonov@st.noorderpoort.nl>
2025-03-11 13:25:21 +0100

    swirl image import gefixt

--- template/components/navbar.php
+++ template/components/navbar.php
@@ -1,6 +1,6 @@
 <nav>
     <div class="container mx-auto flex justify-between items-center">
-        <img src="../public/images/swirl2.png" class="h-20 mb-2" alt="swirl">
+        <img src="images/swirl2.png" class="h-20 mb-2" alt="swirl">
         <div class="hidden sm:block">
             <div class="flex space-x-4">
                 <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-[#36B843]">Over ons</a>
