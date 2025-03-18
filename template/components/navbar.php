a9388c86f3a46471285358f836e7d0c3ffaf425a
ramon <rpvarwijk@st.noorderpoort.nl>
2025-03-18 11:11:42 +0100

    proberen om te fixen

--- template/components/navbar.php
+++ template/components/navbar.php
@@ -1,12 +1,12 @@
 <nav>
     <div class="container mx-auto flex justify-between items-center">
-        <img src="../public/images/swirl2.png" class="h-20 mb-2" alt="swirl">
+        <img src="images/swirl2.png" class="h-20 mb-2" alt="swirl">
         <div class="hidden sm:block">
             <div class="flex space-x-4">
-                <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-[#36B843]">Over ons</a>
-                <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-[#36B843]">Stage</a>
-                <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-[#36B843]">Partners</a>
-                <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-[#36B843]">Contact</a>
+                <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-primary-color">Over ons</a>
+                <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-primary-color">Stage</a>
+                <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-primary-color">Partners</a>
+                <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-primary-color">Contact</a>
             </div>
         </div>
     </div>
