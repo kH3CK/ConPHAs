7a410d6bf019932d4505919b3fbada921eb30d4b
Jason van Ark <jpvanark@st.noorderpoort.nl>
2025-03-18 11:16:27 +0100

    lijn

--- template/views/over.php
+++ template/views/over.php
@@ -3,27 +3,46 @@
 require_once("../template/components/head.php");
 
 ?>
+<div>
+  <nav class="">
+    <div class="container mx-auto flex justify-between items-center">
 
-<body>
-  <div class="bg-[url('/public/images/bg.png')] bg-cover bg-center h-screen w-full relative">
-    <img src="images/bg.png" class="absolute inset-0 w-full h-full object-cover" alt="line">
-  </div>
+
+  </nav>
+
+  <!-- <div class="bg-[url('/public/images/bg.png')] bg-cover bg-center h-screen w-full relative">
+    <img src="images/bg.png" class="absolute inset-0 w-full h-full object-cover opacity-60" alt="line">
+    <div class="absolute top-0 right-0 m-4 hidden sm:block">
+      <div class="flex space-x-4">
+        <a href="#" class="rounded-md px-3 py-2 text-sm font-bold" style="color: #37852D;">Over ons</a>
+        <a href="#" class="rounded-md px-3 py-2 text-sm font-bold" style="color: #37852D;">Stage</a>
+        <a href="#" class="rounded-md px-3 py-2 text-sm font-bold" style="color: #37852D;">Partners</a>
+        <a href="#" class="rounded-md px-3 py-2 text-sm font-bold" style="color: #37852D;">Contact</a>
+      </div>
+    </div>
+  </div> -->
   <div class="absolute top-60 left-0 w-full h-full opacity-50">
-    <svg
-      viewBox="0 0 500 100"
-      className="w-full h-20"
-      xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M 0 50 Q 50 0, 100 50 T 200 50 T 300 50 T 400 50 T 500 50"
-        stroke="black"
-        fill="transparent"
-        strokeWidth="2" />
-    </svg>
-  </div>
-  <div class="relative inline-block group">
-    <img src="images/fles.png" alt="fles" class="hover-trigger">
-    <div class="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-100 bg-black text-white text-center rounded-lg py-1 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-      Plastic word voor van alles gebruikt en vervuilt onze aarde. Dit is natuurlijk niet goed, daarom maken wij een materiaal wat lijkt op plastic. Dit is gemaakt van Polyhydroxyalkanoaten (PHA's). Die worden in de natuur geproduceerd door een grote hoeveelheid aan micro-organismen, waaronder bacteriën. Hiermee kan bioplastic gemaakt worden en dit is biologisch afbreekbaar.
+<div class="process-line h-full"></div>
+    <div class="flex justify-between">
+      <div class="w-32 h-32 bg-green-500 rounded-full flex items-center justify-center -mt-60 z-10">
+      <span class="text-white font-bold">Circle</span>
+      </div>
+      <div class="w-32 h-32 bg-green-500 rounded-full flex items-center justify-center -mt-36 -ml-65">
+      <span class="text-white font-bold">Circle</span>
+      </div>
+      <div class="w-32 h-32 bg-green-500 rounded-full flex items-center justify-center -mt-36 -mr-64">
+      <span class="text-white font-bold">Circle</span>
+      </div>
+      <div class="w-32 h-32 bg-green-500 rounded-full flex items-center justify-center -mt-60">
+      <span class="text-white font-bold">Circle</span>
+      </div>
+    </div>
+    <i class="fa-solid fa-bottle-water"></i>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div class="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-100 bg-black text-white text-center rounded-lg py-1 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+        Plastic word voor van alles gebruikt en vervuilt onze aarde. Dit is natuurlijk niet goed, daarom maken wij een materiaal wat lijkt op plastic. Dit is gemaakt van Polyhydroxyalkanoaten (PHA's). Die worden in de natuur geproduceerd door een grote hoeveelheid aan micro-organismen, waaronder bacteriën. Hiermee kan bioplastic gemaakt worden en dit is biologisch afbreekbaar.
+      </div>
     </div>
   </div>
+</div>
 </body>
