2e140f5d10e0c54ad17cbd8df4e2866812f2b424
ramon <rpvarwijk@st.noorderpoort.nl>
2025-03-21 11:42:29 +0100

    stageplekken

--- template/views/stageplekken.php
+++ template/views/stageplekken.php
@@ -1,21 +1,10 @@
 <?php
 
 require_once("../template/components/head.php");
-
+require_once("../template/components/navbar.php");
 
 ?>
-<nav>
-    <div class="container mx-auto flex justify-end items-center flex-start">
-        <div class="hidden sm:block">
-            <div class="flex space-x-4">
-                <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-primary-color">Over ons</a>
-                <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-primary-color">Stage</a>
-                <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-primary-color">Partners</a>
-                <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-primary-color">Contact</a>
-            </div>
-        </div>
-    </div>
-</nav>
+
 <div class="max-w-6xl mx-auto flex flex-col md:flex-row gap-6 p-6">
         <div class="w-full md:w-1/4 bg-green-100 p-4 rounded-lg">
             <h2 class="font-bold">Filters</h2>
