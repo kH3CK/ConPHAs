225a6782d9041f2aa5dcd3b1814e8969f80bc6fe
Jason van Ark <jpvanark@st.noorderpoort.nl>
2025-04-08 14:09:01 +0200

    joe

--- template/views/stage_details.php
+++ template/views/stage_details.php
@@ -45,13 +45,8 @@ require_once("../template/components/navbar.php");
                     <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
                 </svg>
                 <div class="flex flex-col">
-<<<<<<< HEAD
-                    <span>miniumnodigniveau</span>
-                    <span>3</span>
-=======
                     <span>Minium niveau</span>
                     <span>Niveau 3</span>
->>>>>>> jason
                 </div>
             </div>
             <div class="flex items-center space-x-2">
@@ -59,11 +54,7 @@ require_once("../template/components/navbar.php");
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
                 </svg>
                 <div class="flex flex-col">
-<<<<<<< HEAD
-                    <span>typeStage</span>
-=======
                     <span>type Stage</span>
->>>>>>> jason
                     <span>Afstudeerstage</span>
                 </div>
             </div>
