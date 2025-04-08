2d35983bf859d75ce48ccfbd70bb34a1cc85ec65
Jason van Ark <jpvanark@st.noorderpoort.nl>
2025-04-08 13:32:55 +0200

    stageplekkenaanpas

--- template/views/stageplekken.php
+++ template/views/stageplekken.php
@@ -13,10 +13,40 @@ require_once("../template/components/navbar.php");
                     <img src="images/onderzoek.png" alt="Stage" class="w-20 h-20 rounded-md object-cover">
                     <div class="ml-4">
                         <h3 class="font-bold">[stagePlekNaam]</h3>
-                        <p class="text-sm">📅 [startDatum] [aantalWeken] | [urenT/Week]</p>
-                        <p class="text-sm">📍 [provincie] | [stad]</p>
-                        <p class="text-sm">✅ [minimumNodigNiveau]</p>
-                        <p class="text-sm">🎓 [typeStage]</p>
+                        <div class="flex flex-col text-sm">
+                            <div class="flex flex-row gap-4">
+                                <div class="flex flex-col">
+                                    <span>📅 Start Datum = 3 | </span>
+                                </div>
+                                <div class="flex flex-col">
+                                    <span>Aantal Weken = 3 |</span>
+                                </div>
+                                <div class="flex flex-col">
+                                    <span>Uren T/Week = 3</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col text-sm">
+                            <div class="flex flex-row gap-4">
+                                <div class="flex flex-col">
+                                    <span>📍 Locatie = Groningen</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col text-sm">
+                            <div class="flex flex-row gap-4">
+                                <div class="flex flex-col">
+                                    <span>✅ Minimum Niveau = Niveau 3</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col text-sm">
+                            <div class="flex flex-row gap-4">
+                                <div class="flex flex-col">
+                                    <span>🎓 type Stage = Afstudeerstage</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="flex items-center bg-green-50 p-4 rounded-lg shadow-md" repeat="10">
@@ -115,27 +145,17 @@ require_once("../template/components/navbar.php");
             <h2 class="font-bold">Filters</h2>
             <input type="text" placeholder="Trefwoord" class="w-full p-2 mt-2 border rounded">
             <select class="w-full p-2 mt-2 border rounded">
-            <option>beroepsgroep</option>
+                <option>beroepsgroep</option>
             </select>
             <select class="w-full p-2 mt-2 border rounded">
-            <option>opleidings niveau</option>
+                <option>opleidings niveau</option>
             </select>
             <select class="w-full p-2 mt-2 border rounded">
-            <option>locatie</option>
+                <option>locatie</option>
             </select>
             <h2>stage soort</h2>
 
 
-            <label class="relative inline-flex items-center cursor-pointer">
-            <input type="checkbox" class="sr-only peer">
-            <div class="w-11 h-6 bg-gray-300 peer-focus:ring-4 peer-focus:ring-green-300 
-                rounded-full peer peer-checked:bg-green-500 
-                transition duration-300"></div>
-            <div class="absolute left-1 top-1 w-4 h-4 bg-white rounded-full shadow 
-                peer-checked:translate-x-5 transition duration-300"></div>
-            <span class="ml-2">afstudeerstage</span>
-            </label>
-            <div class="div">
             <label class="relative inline-flex items-center cursor-pointer">
                 <input type="checkbox" class="sr-only peer">
                 <div class="w-11 h-6 bg-gray-300 peer-focus:ring-4 peer-focus:ring-green-300 
@@ -143,45 +163,55 @@ require_once("../template/components/navbar.php");
                 transition duration-300"></div>
                 <div class="absolute left-1 top-1 w-4 h-4 bg-white rounded-full shadow 
                 peer-checked:translate-x-5 transition duration-300"></div>
-                <span class="ml-2">meewerkstage</span>
+                <span class="ml-2">afstudeerstage</span>
             </label>
+            <div class="div">
+                <label class="relative inline-flex items-center cursor-pointer">
+                    <input type="checkbox" class="sr-only peer">
+                    <div class="w-11 h-6 bg-gray-300 peer-focus:ring-4 peer-focus:ring-green-300 
+                rounded-full peer peer-checked:bg-green-500 
+                transition duration-300"></div>
+                    <div class="absolute left-1 top-1 w-4 h-4 bg-white rounded-full shadow 
+                peer-checked:translate-x-5 transition duration-300"></div>
+                    <span class="ml-2">meewerkstage</span>
+                </label>
             </div>
             <div class="">
-            <label class="block text-gray-700">Stage start maand</label>
-            <select class="w-full p-2 mt-2 border rounded"> 
-                <option>januari</option>
-                <option>februari</option>
-                <option>maart</option>
-                <option>april</option>
-                <option>mei</option>
-                <option>juni</option>
-                <option>juli</option>
-                <option>augustus</option>
-                <option>september</option>
-                <option>oktober</option>
-                <option>november</option>
-                <option>december</option>
-            </select>
-            <label class="block text-gray-700">Stage duur</label>
-            <div class="flex items-center gap-2">
-            <input type="range" min="1" max="52" class="w-full accent-green-500">
-            <span class="text-gray-700 w-16 text-left">weken</span>
-            </div>
-            <div class="">
-            <label class="block text-gray-700">Zoek radius</label>
-            <div class="flex items-center gap-2">
-                <input type="range" min="1" max="100" class="w-full accent-green-500">
-                <span class="text-gray-700 w-16 text-left">km</span>
-            </div>
-            </div>
-            <div>
-            <label class="block text-gray-700">Stagevergoeding</label>
-            <div class="flex items-center gap-2">
-                <input type="range" min="0" max="2000" class="w-full accent-green-500">
-                <span class="text-gray-700 w-16 text-left">€</span>
-            </div>
+                <label class="block text-gray-700">Stage start maand</label>
+                <select class="w-full p-2 mt-2 border rounded">
+                    <option>januari</option>
+                    <option>februari</option>
+                    <option>maart</option>
+                    <option>april</option>
+                    <option>mei</option>
+                    <option>juni</option>
+                    <option>juli</option>
+                    <option>augustus</option>
+                    <option>september</option>
+                    <option>oktober</option>
+                    <option>november</option>
+                    <option>december</option>
+                </select>
+                <label class="block text-gray-700">Stage duur</label>
+                <div class="flex items-center gap-2">
+                    <input type="range" min="1" max="52" class="w-full accent-green-500">
+                    <span class="text-gray-700 w-16 text-left">weken</span>
+                </div>
+                <div class="">
+                    <label class="block text-gray-700">Zoek radius</label>
+                    <div class="flex items-center gap-2">
+                        <input type="range" min="1" max="100" class="w-full accent-green-500">
+                        <span class="text-gray-700 w-16 text-left">km</span>
+                    </div>
+                </div>
+                <div>
+                    <label class="block text-gray-700">Stagevergoeding</label>
+                    <div class="flex items-center gap-2">
+                        <input type="range" min="0" max="2000" class="w-full accent-green-500">
+                        <span class="text-gray-700 w-16 text-left">€</span>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
 
-</div>
+    </div>
