9bcd45feb2102c4e5b671079789f2bb9611ee30c
ramon <rpvarwijk@st.noorderpoort.nl>
2025-04-15 11:18:20 +0200

    sql

--- import.sql
+++ import.sql
@@ -46,7 +46,7 @@ INSERT INTO internships (title, description, minimum_level, location, weeks, hou
 "https://picsum.photos/200/200"), ("Natuurbehoud", "Lorem ipsum dolor sit amet", "MBO 4", "Groningen, friesestraatweg", 54, 500, "2026-02-01 08:30:00", "Meewerkstage",
 "https://picsum.photos/200/200"), ("Schoonmaker", "Lorem ipsum dolor sit amet", "MBO 1", "Assen, Weiersstraat", 10, 90, "2025-06-15 10:00:00", "Meewerkstage",
 "https://picsum.photos/200/200"), ("Laboriant", "Lorem ipsum dolor sit amet", "MBO 3", "Assen, Thorbeckelaan", 35, 330, "2025-08-01 09:30:00", "afstudeerstage",
-"https://picsum.photos/200/200"), ("Bioloog", "Lorem ipsum dolor sit amet", "MBO 4", "Leeuwarden, Groeneweg", 30, 250, "2026-03-01 08:30:00", "afstudeerstage",
+"https://picsum.photos/200/200"), ("Bioloog", "Lorem ipsum dolor sit amet", "HBO", "Leeuwarden, Groeneweg", 30, 250, "2026-03-01 08:30:00", "afstudeerstage",
 "https://picsum.photos/200/200");
 
 CREATE TABLE trainees (
