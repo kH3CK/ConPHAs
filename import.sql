27ee4ae89b2f1d60be2728e61ead5fabdc3c6fb9
Jason van Ark <jpvanark@st.noorderpoort.nl>
2025-04-22 13:56:37 +0200

    stagedetails

--- import.sql
+++ import.sql
@@ -39,14 +39,14 @@ CREATE TABLE internships (
 
 INSERT INTO internships (title, description, minimum_level, location, weeks, hours, start_date_and_time, type, image_link) VALUES
 ("Bioloog", "Lorem ipsum dolor sit amet", "MBO 3", "Groningen, Muntinglaan 5", 50, 300, "2025-09-01 09:00:00", "Afstudeerstage",
-"https://picsum.photos/200/200"), ("Laboriant", "Lorem ipsum dolor sit amet", "MBO 4", "Groningen, Jan Steenstraat 12", 40, 400, "2025-11-20 11:00:00", "Afstudeerstage",
-"https://picsum.photos/200/200"), ("Milieudeskundige", "Lorem ipsum dolor sit amet", "MBO 3", "Groningen, Friesestraatweg 5", 54, 350, "2026-02-01 08:30:00", "Meewerkstage",
-"https://picsum.photos/200/200"), ("Wetenschapsvoorlichter", "Lorem ipsum dolor sit amet", "MBO 4", "Groningen, Johan Willem Frisostraat 26", 20, 100, "2025-05-23 08:00:00", "Meewerkstage",
-"https://picsum.photos/200/200"), ("Natuurbehoud", "Lorem ipsum dolor sit amet", "MBO 4", "Groningen, friesestraatweg", 54, 500, "2026-02-01 08:30:00", "Meewerkstage",
-"https://picsum.photos/200/200"), ("Schoonmaker", "Lorem ipsum dolor sit amet", "MBO 1", "Assen, Weiersstraat", 10, 90, "2025-06-15 10:00:00", "Meewerkstage",
-"https://picsum.photos/200/200"), ("Laboriant", "Lorem ipsum dolor sit amet", "MBO 3", "Assen, Thorbeckelaan", 35, 330, "2025-08-01 09:30:00", "afstudeerstage",
-"https://picsum.photos/200/200"), ("Bioloog", "Lorem ipsum dolor sit amet", "HBO", "Leeuwarden, Groeneweg", 30, 250, "2026-03-01 08:30:00", "afstudeerstage",
-"https://picsum.photos/200/200");
+"images/biotechnoloog.jpg"), ("Laborant", "Lorem ipsum dolor sit amet", "MBO 4", "Groningen, Jan Steenstraat 12", 40, 400, "2025-11-20 11:00:00", "Afstudeerstage",
+"images/shutterstock_685086673"), ("Milieudeskundige", "Lorem ipsum dolor sit amet", "MBO 3", "Groningen, Friesestraatweg 5", 54, 350, "2026-02-01 08:30:00", "Meewerkstage",
+"images/headerm25-3"), ("Wetenschapsvoorlichter", "Lorem ipsum dolor sit amet", "MBO 4", "Groningen, Johan Willem Frisostraat 26", 20, 100, "2025-05-23 08:00:00", "Meewerkstage",
+"images/presentatievaardigheden-toolshero"), ("Natuurbehoud", "Lorem ipsum dolor sit amet", "MBO 4", "Groningen, friesestraatweg", 54, 500, "2026-02-01 08:30:00", "Meewerkstage",
+"images/natuur.jpg"), ("Schoonmaker", "Lorem ipsum dolor sit amet", "MBO 1", "Assen, Weiersstraat", 10, 90, "2025-06-15 10:00:00", "Meewerkstage",
+"images/schoonmaker.jpg"), ("Laborant", "Lorem ipsum dolor sit amet", "MBO 3", "Assen, Thorbeckelaan", 35, 330, "2025-08-01 09:30:00", "afstudeerstage",
+"images/lab.jpg"), ("Bioloog", "Lorem ipsum dolor sit amet", "HBO", "Leeuwarden, Groeneweg", 30, 250, "2026-03-01 08:30:00", "afstudeerstage",
+"images/biloog.jpg");
 
 CREATE TABLE trainees (
     id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
