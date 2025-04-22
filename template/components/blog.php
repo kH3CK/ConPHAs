3509f9028ecde40ded8a5afd53fd6ec4f477ca32
Oleksandr Antonov <ooantonov@st.noorderpoort.nl>
2025-04-22 10:03:07 +0200

    lees meer weggehaald vanuit blog details

--- template/components/blog.php
+++ template/components/blog.php
@@ -16,7 +16,7 @@ $months = [
     "december"
 ];
 
-function makeBlog(array $blog, $previewText = true) {
+function makeBlog(array $blog, $preview = true) {
     $dateandtime = $blog["publication_date"];
     $splitdateandtime = explode(" ", $dateandtime);
     $splittime = explode(":", $splitdateandtime[1]);
@@ -39,14 +39,20 @@ function makeBlog(array $blog, $previewText = true) {
         </div>
         <h3 class="text-xl font-bold text-gray-900 mb-2"><?=$blog["title"]?></h3>
         <p class="text-gray-600 mb-4">
-            <?=$blog[($previewText ? "preview_": "") . "text"]?>
+            <?=$blog[($preview ? "preview_": "") . "text"]?>
         </p>
+        <?php
+
+if ($preview) {?>
         <a href="/blog_details?id=<?=$blog["id"]?>" class="inline-flex items-center text-conphas-green hover:text-green-700">
             Lees meer
             <svg class="ml-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
             </svg>
         </a>
+<?php }
+
+        ?>
     </div>
 </article>
 <?php }
