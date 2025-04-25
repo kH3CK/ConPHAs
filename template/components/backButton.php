3367705bb88b19cd9b5fe0eafb247df800fbe56c
Oleksandr Antonov <ooantonov@st.noorderpoort.nl>
2025-04-25 12:35:22 +0200

    backbutton verplaatst naar edit_internships want het wat gebruikt alleen daar en in de nu verwijderde edit_internships

--- template/components/backButton.php
+++
@@ -1,3 +0,0 @@
-<div class="sticky bottom-2 left-2 text-start m-2 max-w-30 pointer-events-none">
-    <a href="/admin" class="pointer-events-auto"><img src="images/arrow-left.png" alt="<-" class="inline w-1/2 h-1/2"> terug</a>
-</div>
--- template/views/edit_internships.php
+++ template/views/edit_internships.php
@@ -156,5 +156,7 @@ require_once("../template/components/editableInternship.php");
         </form>
     </div>
 </div>
-<?php require_once("../template/components/backButton.php"); ?>
+<div class="sticky bottom-2 left-2 text-start m-2 max-w-30 pointer-events-none">
+    <a href="/admin" class="pointer-events-auto"><img src="images/arrow-left.png" alt="<-" class="inline w-1/2 h-1/2"> terug</a>
+</div>
 <script src="js/abandonChanges.js"></script>
